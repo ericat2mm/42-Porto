93ea754e178a2d5fd47f6209aa09fb3e5b373312
ericat2mm <133584039+ericat2mm@users.noreply.github.com>
2023-09-07 11:09:12 +0100

    Update ft_strstr.c

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -17,18 +17,28 @@ char	*ft_strstr(char *str, char *to_find)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && to_find[i] != '\0')
-	{
-		if (str[i] == to_find[i])
-			return (&str[i]);
-		i++;
-	}
+	while (str[i] != '\0')
+		{
+			j = 0;
+			if (str[i] == to_find[j])
+			{
+				while (to_find[i] != '\0')
+					{
+						while (str[i + j] == to_find[j])
+							j++;
+						if (to_find[j] == '\0')
+							return (&str[i]);
+						break;
+					}
+			}
+			i++;
+		}
 	return (&str[i]);
 }
 
-int	main(int argc, char **argv)
+/*int	main(int argc, char **argv)
 {
 	if (argc == 3)
 		printf("%s", ft_strstr(argv[1], argv[2]));
-}
+}*/
 
