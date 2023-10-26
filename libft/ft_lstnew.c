12f00e049e5f1ca41c1813705b93eb233e6cdb23
Erica Tamara Medeiros Marques <emedeiro@c1r8s3.42porto.com>
2023-10-26 16:42:59 +0100

    libft

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emedeiro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 09:38:36 by emedeiro          #+#    #+#             */
+/*   Updated: 2023/10/26 11:33:47 by emedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*result;
+
+	result = (t_list *)malloc(sizeof(*result));
+	if (!result)
+		return (0);
+	result->content = content;
+	result->next = 0;
+	return (result);
+}
